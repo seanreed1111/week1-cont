f10ea7e51af270b43c69467540e610cf6c702583
Sean Reed <seanreed1111@gmail.com>
2014-06-03 14:53:23 -0400

    Initial Commit

---
+++ week1-cont/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  week1-cont
+//
+//  Created by Sean Reed on 6/3/14.
+//  Copyright (c) 2014 seanreed.test. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
