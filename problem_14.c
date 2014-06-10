68692e93ae004a649da10332c3a1d15c45011b8e
Sean Reed <seanreed1111@gmail.com>
2014-06-10 17:38:32 -0400

    adds problem 14 and 15

--- problem_14.c
+++ problem_14.c
@@ -1,19 +1,19 @@
 //
-//  problem_13.c
+//  problem_14.c
 //  week1-cont
 //
 //  Created by Sean Reed on 6/10/14.
 //  Copyright (c) 2014 seanreed.test. All rights reserved.
 //
 
-//13. Read a file and count the number of lines in that program
 //14. Modify 13 to count the number of lines of comment marked with //
 //15. Modify 14 to include /* */ style of comments
 //
 #include <stdio.h>
 
-int main(int argc, char* argv[]){
 
+int main(int argc, char* argv[]){
+    
     int c;
     int ct = 0;
     FILE *fp;
@@ -21,14 +21,21 @@ int main(int argc, char* argv[]){
     
     if((fp = fopen(filename, "r"))){
         
-        // loop through to count number of newlines
+        // loop through to count number of '//' comments
         
         while ((c = getc(fp)) != EOF){
-            if (c == '\n') {ct++;}
+            if (c == '/') {
+                if ((c=getc(fp)) == '/'){
+                    ct++;
+                }
+            }
+            if (c == EOF){
+                break;
+            }
         }
         
         fclose(fp);
-        printf("The file has %d lines\n", ct);
+        printf("The file has %d commented lines\n", ct);
         return 0;
     }
 }
